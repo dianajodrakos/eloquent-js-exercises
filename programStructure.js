c06d1e9fadc1bdf15f1aa6b46af2f020403f8581
DJ Drakos <dianajodrakos@gmail.com>
2021-11-11 15:07:05 -0800

    created chessboard function, test passes

--- programStructure.js
+++ programStructure.js
@@ -22,8 +22,47 @@ const fizzBuzz = (n) => {
 };
 
 const chessboard = () => {
+//takes in a number 8
+//should create a string that's a grid of 8x8 and log it to the console
 
-}
+//odd squares are ' '
+//even squares are '#'
+
+//odd rows and columns also start with ' '
+//even rows and columns also start with '#'
+
+//the easy way to do this would be to use a loop and add to the string on each loop, but processing time might be slow for very large numbers
+
+//can we use math stuff to expedite this process?
+
+//let's start with a loop
+const grid = 8;
+let board = '';
+let oddRow = '';
+
+  for (let i = 1; i <= grid; i++) {
+    if (i % 2) {
+      oddRow = oddRow + ' ';
+    } else {
+      oddRow = oddRow + '#';
+    }
+  }
+
+  const evenRow = oddRow.slice(1, grid).concat(oddRow.slice(0, 1));
+
+  for (let i = 1; i <= grid; i++) {
+    if (i % 2) {
+      board = board + oddRow;
+    } else {
+      board = board + evenRow;
+    }
+    if (i < grid) {
+      board = board + '\n';
+    }
+  }
+
+  console.log(board);
+};
 
 module.exports = { 
   generatePyramid,
